957f92786a8ea0f960b572db3d3eccc26ec07bb6
tskarhed <1438972+tskarhed@users.noreply.github.com>
2021-03-13 22:14:04 +0100

    Fix post styles

--- .eleventy.js
+++ .eleventy.js
@@ -68,6 +68,7 @@ module.exports = function(eleventyConfig) {
 
   eleventyConfig.addPassthroughCopy("img");
   eleventyConfig.addPassthroughCopy("css");
+  eleventyConfig.addPassthroughCopy("css/fonts");
 
   /* Markdown Overrides */
   let markdownLibrary = markdownIt({
